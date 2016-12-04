09bdfd8b1aaf2710a75c1490f176e9d17271de69
Lee Holmes <leeholmesmail+github@gmail.com>
2016-12-04 16:51:21 +0000

    Day 3 part 2

--- Day 3 - Squares With Three Sides.fsx
+++ Day 3 - Squares With Three Sides.fsx
@@ -16,6 +16,22 @@ let possibleTriangle sides =
     else
         sides |> Array.sortDescending |> fun xs -> xs.[0] < xs.[1] + xs.[2]
 
+
+/// pull 3-element columns from a sequence of rows
+let transposeToColumns rows =
+    rows
+    |> Seq.chunkBySize 3
+    |> Seq.collect (fun xs -> Seq.zip3 xs.[0] xs.[1] xs.[2])
+    |> Seq.map (fun (x,y,z) -> [|x;y;z|])
+
+
+/// Count the possible triangles in a sequence of side groups
+let countPossibleTris sideGroups =
+    sideGroups
+    |> Seq.filter possibleTriangle
+    |> Seq.length
+    |> printfn "%d possible triangles"
+
 (*
     Runnables
 *)
@@ -23,17 +39,37 @@ let possibleTriangle sides =
 let part1() =
     getInput()
     |> Array.map parseInts
-    |> Seq.filter possibleTriangle
-    |> Seq.length
-    |> printfn "%d possible triangles"
+    |> countPossibleTris
+
+
+let part2() =
+    getInput()
+    |> Array.map parseInts
+    |> transposeToColumns
+    |> countPossibleTris
 
 
 let test1() =
     let input = "5 10 25"
     let expected = false
     let result = parseInts input |> possibleTriangle
-    printfn "Test %s" <| if result = expected then "passed" else "failed"
-    
+    printfn "Test 1 %s" <| if result = expected then "passed" else "failed"
+
+
+let testTranspose() =
+    let input =
+        [| [|101; 201; 301|];
+           [|102; 202; 302|];
+           [|103; 203; 303|] |]
+    let expected =
+        [| [|101; 102; 103|];
+           [|201; 202; 203|];
+           [|301; 302; 303|] |]
+    let result = input |> Seq.ofArray |> transposeToColumns |> Array.ofSeq
+    printfn "Transpose test %s" <| if result = expected then "passed" else "failed"
+
 
 part1()
+part2()
 test1()
+testTranspose()
