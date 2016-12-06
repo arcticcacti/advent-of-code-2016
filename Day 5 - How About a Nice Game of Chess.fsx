0115f52be571870354b7e9fa37d3523a84693c4a
Lee Holmes <leeholmesmail+github@gmail.com>
2016-12-06 14:19:08 +0000

    Day 5 part 2

--- Day 5 - How About a Nice Game of Chess.fsx
+++ Day 5 - How About a Nice Game of Chess.fsx
@@ -15,36 +15,98 @@ let hashID id index =
     |> Seq.reduce (+)
 
 
-/// crack the password for a given door ID
-let crackId doorId =
+/// get a valid password character and its position
+let passwordChar passwordLength (hash:string) =
+    // 6th and 7th chars hold the index and char respectively
+    let i = hash.[5]
+    let c = hash.[6]
+    match System.Int32.TryParse(string i) with
+    | true, x when x >= 0 && x <= passwordLength-1
+        -> Some(x, c)
+    | true, _
+        -> None
+    | _ 
+        -> None
+
+
+/// generate valid hashes for a given door ID
+let validHashes doorId =
     let hash = hashID doorId
     let fiveLeadingZeroes str = str |> Seq.take 5 |> Seq.forall ((=) '0')
 
-    printf "Getting code: "
     Seq.initInfinite id
     |> Seq.map hash
     |> Seq.filter fiveLeadingZeroes
+
+let printCharInline = fun x -> printf "%c" x; x
+
+let charsToString = Array.ofSeq >> fun arr -> new System.String(arr)
+
+
+(*
+    Main functions
+*)
+
+let part1 input =
+    let codeChar (hash:string) = hash.[5]
+
+    printf "Getting code: "
+    input
+    |> validHashes
     |> Seq.take 8
-    |> Seq.map (fun md5Hash -> md5Hash.[5])
+    |> Seq.map (codeChar >> printCharInline)
+    |> charsToString
+    |> fun x -> printfn "\nDone"; x
 
 
-let part1() =
-    getInput()
-    |> crackId
-    |> Seq.iter (fun x -> printf "%c" x)
-    printfn "\nDone"
+let part2 input =
+    let setChar array (idx, ch) = Array.set array idx ch; array
+    /// display a 'live' view of the password array that updates as entries are added
+    let setCharAndDisplayProgress array =
+        setChar array >> fun x -> printf "\r[%s]" (charsToString x); x
 
+    let passwordLength = 8
+
+    // Get valid password index/char pairs, and combine the first result for each index into an array
+    printfn "Getting code: "
+    input
+    |> validHashes
+    |> Seq.choose (passwordChar passwordLength)
+    |> Seq.distinctBy fst
+    |> Seq.take passwordLength
+    |> Seq.fold setCharAndDisplayProgress (Array.zeroCreate passwordLength)
+    |> charsToString
+    |> fun x -> printfn "\nDone"; x
+
+
+(*
+    Tests
+*)
+
+
+let printTestResult testId result expected =
+    if result = expected then
+        printfn "Test %A passed" testId
+    else
+        printfn "Test %A failed - got: %s" testId result
 
 let test1() =
     let doorId = "abc"
     let expected = "18f47a30"
-    let result = crackId doorId |> Array.ofSeq |> fun x -> new System.String(x)
-    if result.ToLower() = expected then
-        printfn "Test 1 passed"
-    else
-        printfn "Test 2 failed - got: %s" result
+    let result = part1 doorId
+    printTestResult 1 (result.ToLower()) expected
+
+let test2() =
+    let doorId = "abc"
+    let expected = "05ace8e3"
+    let result = part2 doorId
+    printTestResult 2 (result.ToLower()) expected
 
 
-part1()
-// test1()
+
+
+part1 <| getInput()
+part2 <| getInput()
+test1()
+test2()
 1
